c6eabfd81a8ee15a7a93df92e17710d0dc750d90
ThiagoMR <thiagomarzari1@gmail.com>
2025-12-19 14:19:54 -0300

    up

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
-import Header from './components/aprendendo/Header.vue'
-import MultiplosEventos from './components/aprendendo/MultiplosEventos.vue'
-import Pessoa from './components/aprendendo/Pessoa.vue'
-</script>
-
 <template>
   <Header :isSigned="false" />
   <Pessoa />
 
   <MultiplosEventos firstText="texto 1" secondText="texto 2" :delay="500" />
 </template>
+
+<script setup lang="ts">
+import Header from './components/aprendendo/Header.vue'
+import MultiplosEventos from './components/aprendendo/MultiplosEventos.vue'
+import Pessoa from './components/aprendendo/Pessoa.vue'
+</script>
